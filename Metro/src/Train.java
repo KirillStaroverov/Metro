b6a3b48852a6edafab6d2262639e2c939abedf55
kstaroverov <kstaroverov@mrkt-kirill.mylands.local>
2018-01-23 16:04:57 +0200

    Depo

--- Metro/src/Train.java
+++ Metro/src/Train.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
 
 public class Train {
 	private int trainId;
 	private int driverId;
 	private int lineId;
 	
+	Driver driver;
+	
+	ArrayList<Wagon> wagon = new ArrayList<>(5);
 	
 	public int getTrainId() {
 		return trainId;
